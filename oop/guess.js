1f5040253c8b241d7f345686f544e8be6df0b73b
Marlon Clay <luciousmc@icloud.com>
2019-04-12 14:54:27 -0700

    finished convrsion to oop

--- oop/guess.js
+++ oop/guess.js
@@ -1,1 +1,37 @@
-guess.js
+class GuessGame{
+    constructor(){
+        this.secretNumber = null;
+        this.handleGuess = this.handleGuess.bind(this);
+    }
+    guess_start(){
+        this.secretNumber = this.pickRandomNumber(1,10);
+        this.attachHandlers();
+    }
+    pickRandomNumber(min, max){
+        return Math.floor(Math.random() * (max-min)) + min;
+    }
+    attachHandlers(){
+        $('#submitGuess').click( this.handleGuess );
+        $('#userGuess').focus( this.clearGuess );
+    }
+    handleGuess(){
+        var userGuess = parseInt( $('#userGuess').val());
+        if(userGuess<1 || userGuess>10){
+            displayResult('invalid range.  Must be between 1 and 10');
+            return;
+        }
+        if(userGuess === this.secretNumber){
+            this.displayResult('you got it!');
+        } else if (userGuess < this.secretNumber){
+            this.displayResult('too low!');
+        } else if (userGuess > this.secretNumber){
+            this.displayResult('too high!');
+        }
+    }
+    clearGuess(){
+        $("#userGuess").val('');
+    }
+    displayResult( message ){
+        $("#display").text( message );
+    }
+}
--- oop/main.js
+++ oop/main.js
@@ -1,7 +1,8 @@
 
 
 $(document).ready(startApp);
-
+var newGame;
 function startApp(){
-	
+	newGame = new GuessGame();
+	newGame.guess_start();
 }
